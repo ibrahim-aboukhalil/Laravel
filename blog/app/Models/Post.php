ff0928ef4913ad09c8092819265633bab7bdb624
Ibra95ak-pomechain <100825257+ibrahim-aboukhalil@users.noreply.github.com>
2022-03-25 10:40:26 +0400

    Find a Composer Package for Post Metadata

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -3,25 +3,44 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
-  public static function find($slug)
-  {
-    if (!file_exists($path = resource_path("views\posts\{$slug}.html"))) {
-        throw new ModelNotFoundException();
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
     }
-    return cache()->remember("posts\{$slug}",5,function () use($path){
-      return file_get_contents($path);
-    });
+
+    public static function find($slug)
+  {
+      return static::all()->firstWhere('slug', $slug);
   }
 
   public static function all()
   {
-    $files = File::files(resource_path("views/posts/"));
-    return array_map(function ($file)
-    {
-      return $file->getContents();
-    },$files);
+      return collect(File::files(resource_path("views/posts/")))
+          ->map(function ($file){
+              return YamlFrontMatter::parseFile($file);
+          })
+          ->map(function ($document){
+              return new Post($document->title, $document->excerpt, $document->date, $document->body(), $document->slug);
+          });
+      /*alternative way*/
+//    $posts = array_map(function ($file){
+//        $document = YamlFrontMatter::parseFile($file);
+//        return new Post($document->title, $document->excerpt, $document->date, $document->body(), $document->slug);
+//    },$files);
   }
 }
