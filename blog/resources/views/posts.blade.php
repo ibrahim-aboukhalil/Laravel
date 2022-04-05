7652e985e89c9a3655d6d72c766cb3ca5c3cd320
Ibra95ak-pomechain <ibrahim.aboukhalil95@gmail.com>
2022-04-05 12:59:10 +0400

    Blade: The Absolute Basics

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -6,13 +6,13 @@
     <link rel="stylesheet" href="/app.css">
   </head>
   <body>
-    <?php foreach($posts as $post) :?>
-    <article>
-    <h1><a href="/posts/<?= $post->slug?>"><?= $post->title;?></a></h1>
+    @foreach($posts as $post)
+    <article class="{{ $loop->even ? 'mb-4' : '' }}">
+    <h1><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
         <div>
-            <?= $post->excerpt;?>
+            {{ $post->excerpt }}
         </div>
     </article>
-  <?php endforeach; ?>
+    @endforeach
   </body>
 </html>
