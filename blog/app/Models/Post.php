23dbc55850e98c58903e21081f80043464169512
Ibra95ak-pomechain <ibrahim.aboukhalil95@gmail.com>
2022-04-13 10:52:07 +0400

    Route Model Binding

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -10,4 +10,10 @@ class Post extends Model
     use HasFactory;
     protected $guarded = ['id']; // allow all value except this
     protected $fillable = ['title', 'excerpt', 'body']; // allow value
+
+    /*used to determine what attridute you want to use for routing*/
+//    public function getRouteKeyName()
+//    {
+//        return 'slug';
+//    }
 }
--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     @foreach($posts as $post)
         <article class="{{ $loop->even ? 'mb-4' : '' }}">
-            <h1><a href="/posts/{{ $post->id }}">{!! $post->title !!}</a></h1>
+            <h1><a href="/posts/{{ $post->slug }}">{!! $post->title !!}</a></h1>
             <div>
                 {{ $post->excerpt }}
             </div>
