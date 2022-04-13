c97d7043633da13768543059bb246b458ad2c2fb
Ibra95ak-pomechain <ibrahim.aboukhalil95@gmail.com>
2022-04-13 11:17:34 +0400

    Your First Eloquent Relationship

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -2,6 +2,7 @@
     @foreach($posts as $post)
         <article class="{{ $loop->even ? 'mb-4' : '' }}">
             <h1><a href="/posts/{{ $post->slug }}">{!! $post->title !!}</a></h1>
+            <p><a href="#">{{ $post->category->name }}</a></p>
             <div>
                 {{ $post->excerpt }}
             </div>
