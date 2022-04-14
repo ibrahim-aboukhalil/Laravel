73a806fbfa15ae50402ba3e578a884a489e50a99
Ibra95ak-pomechain <ibrahim.aboukhalil95@gmail.com>
2022-04-14 10:22:56 +0400

    Clockwork, and the N+1 Problem

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -17,8 +17,12 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
+    // show sql queries executed
+//    \Illuminate\Support\Facades\DB::listen(function ($query){
+//        logger($query->sql, $query->bindings);
+//    });
     return view('posts',[
-      'posts' => Post::all()
+      'posts' => Post::with('category')->get()
     ]);
 });
 
