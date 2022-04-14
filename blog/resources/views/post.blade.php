7a1c8fa934af81d564180c4a3c3ff24827d76554
Ibra95ak-pomechain <ibrahim.aboukhalil95@gmail.com>
2022-04-14 09:42:58 +0400

    Show All Posts Associated With a Category

--- blog/resources/views/post.blade.php
+++ blog/resources/views/post.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <article>
         <h1>{!! $post->title !!}</h1>
-        <p><a href="#">{{ $post->category->name }}</a></p>
+        <p><a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         <div>
             {!! $post->body !!}
         </div>
--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -2,7 +2,7 @@
     @foreach($posts as $post)
         <article class="{{ $loop->even ? 'mb-4' : '' }}">
             <h1><a href="/posts/{{ $post->slug }}">{!! $post->title !!}</a></h1>
-            <p><a href="#">{{ $post->category->name }}</a></p>
+            <p><a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
             <div>
                 {{ $post->excerpt }}
             </div>
