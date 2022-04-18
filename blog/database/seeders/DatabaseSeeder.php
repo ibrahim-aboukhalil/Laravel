b0f6178f6a2a45298cd57a14d6c3c507fc5b79f9
Ibra95ak-pomechain <ibrahim.aboukhalil95@gmail.com>
2022-04-18 10:29:54 +0400

    Database Seeding Saves Time

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         User::truncate();
+         Category::truncate();
+         Post::truncate();
+
+         $user = User::factory()->create();
+
+         $personal = Category::create([
+             'name' => 'Personal',
+             'slug' => 'personal'
+         ]);
+         $family = Category::create([
+             'name' => 'Family',
+             'slug' => 'family'
+         ]);
+         $work = Category::create([
+             'name' => 'Work',
+             'slug' => 'work'
+         ]);
+
+         Post::create([
+             'user_id' => $user->id,
+             'category_id' => $family->id,
+             'title' => 'My Family Post',
+             'slug' => 'my-family-post',
+             'excerpt' => 'Test1',
+             'body' => 'Test1 test test test.'
+         ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'Test2',
+            'body' => 'Test2 test test test.'
+        ]);
     }
 }
--- blog/resources/views/post.blade.php
+++ blog/resources/views/post.blade.php
@@ -1,9 +1,11 @@
 <x-layout>
     <article>
         <h1>{!! $post->title !!}</h1>
-        <p><a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>
+            By <a href="#">{{ $post->user->name }}</a> in
+            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         <div>
-            {!! $post->body !!}
+            {{ $post->body }}
         </div>
     </article>
     <a href="/">Go back.</a>
